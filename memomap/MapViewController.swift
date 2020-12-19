b81b03e0b3a1ca9a2541f48efbab75c5b69adaea
Iroha Mizutani <karari_iroha@icloud.com>
2020-12-19 16:52:35 +0900

    map make 2

--- memomap/MapViewController.swift
+++ memomap/MapViewController.swift
@@ -7,34 +7,150 @@
 //
 
 import UIKit
+import MapKit
 
-class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate{
+class MapViewController: UIViewController,MKMapViewDelegate,CLLocationManagerDelegate{
 
     @IBOutlet weak var mapView: MKMapView!
-    var pointAno: MKPointAnnotation = MKPointAnnotation()
-    
-    var locationManager: CLLocationManager!
-    
+        var pointAno = MKPointAnnotation()
+        var locationManager: CLLocationManager!
+        var genre = "タイトル"
+        var subTitle = "サブタイトル"
+        
     override func viewDidLoad() {
-        super.viewDidLoad()
+            super.viewDidLoad()
+
+            //ロケーションマネージャーのセットアップ
+            locationManager = CLLocationManager()
+            locationManager.delegate = self
+            locationManager.requestWhenInUseAuthorization()
+
+            //mapViewDelegate
+            mapView.delegate = self
+
+            //中心点の設定
+            mapView.setCenter(mapView.userLocation.coordinate, animated: true)
+
+            //縮尺の設計
+            let mySpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+            let myRegion = MKCoordinateRegion(center: mapView.userLocation.coordinate, span: mySpan)
+
+            //regionの追加
+            mapView.region = myRegion
+
+            //現在地追従
+            mapView.userTrackingMode = MKUserTrackingMode.follow
+
+            //viewに表示
+            self.view.addSubview(mapView)
+
+            //長押し機能
+            let myLongPress: UILongPressGestureRecognizer =  UILongPressGestureRecognizer()
+                myLongPress.addTarget(self, action: #selector(MapViewController.recognizeLongPress(sender:)))
+
+            //myMapに長押し機能を追加
+            mapView.addGestureRecognizer(myLongPress)
+
+
+
+            }
+    
+    
+    @objc func recognizeLongPress(sender: UILongPressGestureRecognizer) {
+
+                // 長押しの最中に何度もピンを生成しないようにする.
+                if sender.state != UIGestureRecognizer.State.began {
+                    return
+                }
+
+                // 長押しした地点の座標を取得.
+                let location = sender.location(in: mapView)
+
+                // locationをCLLocationCoordinate2Dに変換.
+                let myCoordinate: CLLocationCoordinate2D = mapView.convert(location, toCoordinateFrom: mapView)
+
+                // ピンを生成.
+                let myPin: MKPointAnnotation = MKPointAnnotation()
+
+                // 座標を設定.
+                myPin.coordinate = myCoordinate
+
+                // タイトルを設定.
+                myPin.title = genre
+
+                // サブタイトルを設定.
+                myPin.subtitle = subTitle
+
+                // MapViewにピンを追加.
+                mapView.addAnnotation(myPin)
+            }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+
+            let myPinIdentifier = "PinAnnotationIdentifier"
+            //ピンの生成
+            let myPinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: myPinIdentifier)
+
+            //ピンの登場時のアニメーション
+            myPinView.animatesDrop = true
+
+            //コールアウト(吹き出し)の表示
+            myPinView.canShowCallout = true
+
+            //annnotationの設定
+            myPinView.annotation = annotation
+
+            let btn = UIButton(type: .detailDisclosure)
+
+            btn.addTarget(self, action: #selector(buttonEvent(_:)), for: UIControl.Event.touchUpInside)
+            myPinView.rightCalloutAccessoryView = btn
         
-        
-        //ロケーションマネージャーのセットアップ
-        locationManager = CLLocationMAnager()
-        locationManager.delegate = self
-        locationManager!.requestWhenInUseAuthorization()
-        
-        //デリゲートを設定
-        mapView.delegate = self
-        
-        //経度、緯度を生成.
-        let myLatitude: CLLOcationDegress = 37.331714
-        let myLongitude: CLLOcationDegress = -122.030333
-        
-        //MapViewに中心点を設定
-        mapView.setcenter
-        
-        
+        return myPinView
+            }
+
+            func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+                switch status {
+                //許可されていない場合
+                case .notDetermined:
+                    //許可を求める
+                    manager.requestWhenInUseAuthorization()
+                //拒否されている場合
+                case .denied, .restricted:
+                    break
+                //許可されている場合
+                case .authorizedAlways, .authorizedWhenInUse:
+                    manager.startUpdatingLocation()
+                    break
+
+                default:
+                    break
+                }
+            }
+
+            @objc func buttonEvent(_ sender: UIButton) {
+                self.performSegue(withIdentifier: "next", sender: nil)
+
+
+            }
+    
+            private func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+            switch status {
+            //許可されていない場合
+            case .notDetermined:
+                //許可を求める
+                manager.requestWhenInUseAuthorization()
+            //拒否されている場合
+            case .denied, .restricted:
+                break
+            //許可されている場合
+            case .authorizedAlways, .authorizedWhenInUse:
+                manager.startUpdatingLocation()
+                break
+
+            default:
+                break
+            }
+        }
 
         // Do any additional setup after loading the view.
     }
@@ -50,4 +166,4 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     }
     */
 
-}
+
--- memomap/PreserveViewController.swift
+++ memomap/PreserveViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class PreserveViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource{
     
-    @IBOutlet weak var textField: UITextField!
-    var pickerView = UIPickerView()
-    var data = ["飲食店","観光地","お店","その他"]
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    
+    var pickerView1 = UIPickerView()
+    var pickerView2 = UIPickerView()
+    var data1 = ["飲食店","観光地","お店","その他"]
+    var data2 = ["和食","洋食","中華","イタリアン","フレンチ","パン・サンドイッチ","弁当","焼肉","カレー","カフェ・スイーツ","その他"]
+    var data3 = ["歴史・文化","アウトドア","景観","宿泊","温泉","公園","その他"]
+    var data4 = ["コンビニ","スーパー","ドラッグストア","書店","銀行","カラオケ","その他"]
+    var data5 = ["駐車場","その他"]
+    var word1 = String()
+    var word2 = String()
+    
+    
+    
     
     
     @IBOutlet weak var preservebutton: UIButton!
@@ -23,18 +35,14 @@ class PreserveViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
     }
 
     
-    let innsyokuArray: [String] = ["和食","洋食","中華","イタリアン","フレンチ","パン・サンドイッチ","弁当","焼肉","カレー","カフェ・スイーツ"]
-    let kannkoutiArray: [String] =  ["歴史・文化","アウトドア","景観","宿泊","温泉","公園"]
-    let omiseArray: [String] =  ["コンビニ","スーパー","ドラッグストア","書店","銀行","カラオケ","雑貨屋"]
-    let sonotaArray: [String] =  ["駅・バス停","駐車場","その他"]
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        createPickerView()
+        
         //角丸
         preservebutton.layer.cornerRadius = 40.0
-        
-        createPickerView()
+    
         
         // Do any additional setup after loading the view.
     }
@@ -42,49 +50,137 @@ class PreserveViewController: UIViewController, UIPickerViewDelegate, UIPickerVi
 
     
     func createPickerView() {
-        pickerView.delegate = self
-        textField.inputView = pickerView
-        //toolbar
-        let toolbar = UIToolbar()
-        toolbar.frame = CGRect(x: 0,y: 0,width: self.view.frame.width,height: 44)
-        let doneButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(ViewController.donePicker))
-        toolbar.setItems([doneButtonItem], animated: true)
-        textField.inputAccessoryView = toolbar
         
+        //PickerView1
+        pickerView1.delegate = self
+        textField1.inputView = pickerView1
+        
+        //PickerView2
+        pickerView2.delegate = self
+        textField2.inputView = pickerView2
+        
+        //toolbar1
+        let toolbar1 = UIToolbar()
+        toolbar1.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 44)
+        let doneButtonItem1 = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(PreserveViewController.doneButton1))
+        toolbar1.setItems([doneButtonItem1], animated: true)
+        textField1.inputAccessoryView = toolbar1
+        
+        // toolbar2
+        let toolbar2 = UIToolbar()
+        toolbar2.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 44)
+        let doneButtonItem2 = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(PreserveViewController.doneButton2))
+        toolbar2.setItems([doneButtonItem2], animated: true)
+        textField2.inputAccessoryView = toolbar2
+    
     }
     
-    @objc func donePicker(){
-        textField.endEditing(true)
+    @objc func doneButton1(){
+        textField1.endEditing(true)
+        textField1.text = word1
+        textField2.text = ""
     }
+    
+    @objc func doneButton2() {
+        textField2.endEditing(true)
+        textField2.text = word2
+        }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        textField.endEditing(true)
+        textField1.endEditing(true)
+        textField2.endEditing(true)
+                //テキスト入力画面の外側をタップしたら編集が終了
     }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
+        //1画面に対するクルクル(PickerView)の数
     }
     
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return data.count
-    }
-    
-    internal func pickerView(_ pickerView: UIPickerView,titleForRow row: Int, forComponent component: Int) -> String? {
-        return data[row]
-    }
-    
-    internal func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
-        textField.text = data[row]
-    }
-    
-    /*
-    // MARK: - Navigation
+        //候補の数
+                if pickerView == pickerView1{
+                    return data1.count
+                }else {
+                    if textField1.text == data1[0]{
+                        return data2.count
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+                    }else if textField1.text == data1[1]{
+                        return data3.count
+                        
+                    }else if textField1.text == data1[2]{
+                        return data4.count
 
-}
+                    }else{
+                        return data5.count
+                    }
+                }
+            }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            if pickerView == pickerView1{
+                return data1[row]
+            }else {
+                if textField1.text == data1[0]{
+                    return data2[row]
+                }else if textField1.text == data1[1]{
+                    return data3[row]
+                }else if textField1.text == data1[2]{
+                    return data4[row]
+                }else if textField1.text == data1[3]{
+                    return data5[row]
+                }else{
+                    return ""
+                }
+            }
+        }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+
+            if textField1.text == "" {
+                word1 = data1[row]
+            }
+
+            if textField1.text != "" && textField2.text == ""{
+                if textField1.text == data1[0]{
+                 word2 = data2[row]
+                }else if textField1.text == data1[1]{
+                    word2 = data3[row]
+                }else if textField1.text == data1[2]{
+                    word2 = data4[row]
+                }else if textField1.text == data1[3]{
+                    word2 = data5[row]
+                }else{
+
+                }
+            }
+        
+             if textField1.text != "" && textField2.text != ""{
+                 if pickerView == pickerView1{
+                  word1 = data1[row]
+                }else{
+                        if textField1.text == data1[0]{
+                            word2 = data2[row]
+                        }else if textField1.text == data1[1]{
+                            word2 = data3[row]
+                        }else if textField1.text == data1[2]{
+                            word2 =  data4[row]
+                        }else if textField1.text == data1[3]{
+                            word2 =  data5[row]
+                        }else{
+
+                        }
+                    }
+                }
+
+            }
+    
+    
+
+    }
+    
+    
+    
+    
+
+
